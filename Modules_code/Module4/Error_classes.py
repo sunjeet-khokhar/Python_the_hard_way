5248a90caddf6ffe8cd0b34978237d3b66eda554
sunjeet-khokhar <sunjeet81@gmail.com>
2017-12-19 15:16:19 +1300

    Added a debugging exercise

---
+++ Modules_code/Module4/Error_classes.py
@@ -0,0 +1,15 @@
+class Star:
+    def __init__(self,name,constellation):
+        self.name = name
+        self.constellation = constellation
+    def print_name(self):
+        print("The name of this star is {self.name} ")  
+    def constellation(self):
+        Print(f"The start {self.name} belongs to the {self.constellation} constellation")
+def main():
+    newly_discovered_star = Star('silver_wolf','orion')
+    newly_discovered_star.print_name()
+    newly_discovered_start.constellation()
+if __name__ == '__main__':
+    main()
+
